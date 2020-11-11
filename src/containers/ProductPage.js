95c9b6c8cd734bb367ef1f795388bf6f092a5102
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-11 15:53:25 -0500

    finished styles and added select component

--- src/containers/ProductPage.js
+++ src/containers/ProductPage.js
@@ -6,12 +6,17 @@ import styled from 'styled-components';
 import Spinner from '../components/Spinner';
 import Button from '../components/Button';
 import { addToCart } from '../utils/addToCart';
+import Select from '../components/Select';
 
 const ProductPage = props => {
   const [product, setProduct] = useState({});
+  const [select, setSelect] = useState(1)
   const [loading, setLoading] = useState(false);
   let { id } = useParams();
   const history = useHistory();
+
+  const options = 100;
+  console.log({options})
 
   useEffect(() => {
     setLoading(true);
@@ -28,12 +33,16 @@ const ProductPage = props => {
   }, [id]);
 
   const addToCartHandler = () => {
-    addToCart(id, product.price);
+    addToCart(id, product.price, select);
     history.push('/cart');
   };
 
   // It calls this twice everytime
   // console.log(product);
+
+  const selectChangeHandler = e => {
+    setSelect(e.target.value)
+  }
 
   let prod;
   if (loading) {
@@ -44,7 +53,9 @@ const ProductPage = props => {
         <StyledTitle>{product.title}</StyledTitle>
         <StyledPrice>${product.price}</StyledPrice>
         <StyledImage src={product.image} alt={product.title} />
+        <StyledH3>ABOUT</StyledH3>
         <StyledDesc>{product.description}</StyledDesc>
+        <StyledSelect options={options} changed={selectChangeHandler} />
         <StyledButton clicked={addToCartHandler}>Add to Cart</StyledButton>
       </>
     );
@@ -56,30 +67,56 @@ const ProductPage = props => {
 export default ProductPage;
 
 const StyledDiv = styled.div`
-  margin-top: 2rem;
-  margin-left: 2rem;
-  width: 80%;
+  margin: 2rem 6rem;
+  width: auto;
   display: grid;
-  grid-template-areas: 'img title' 'img price' 'img desc' 'img bttn';
+  grid-template-areas:  'img title .' 
+                        'img price .'
+                        'img bttn bttn'
+                        'img . .'
+                        'about about about'
+                        'desc desc desc';
+
+  @media (max-width: 768px) {
+    grid-template-areas:  'img' 'title' 'price' 'bttn' 'about' 'desc';
+    margin: 2rem;
+  }
 `;
 
 const StyledTitle = styled.h1`
   grid-area: title;
+  text-transform: uppercase;
 `;
 
 const StyledImage = styled.img`
   grid-area: img;
   width: 25rem;
+  place-self: center;
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
 `;
 
 const StyledPrice = styled.h2`
   grid-area: price;
 `;
 
+const StyledH3 = styled.h3`
+  grid-area: about;
+`
+
 const StyledDesc = styled.p`
   grid-area: desc;
 `;
 
+const StyledSelect = styled(Select)`
+  grid-area: bttn;
+  justify-self: start;
+`
+
 const StyledButton = styled(Button)`
   grid-area: bttn;
+  justify-self: center;
+  padding: 0 2rem;
 `;
