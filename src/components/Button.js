5b587ea4ee261fe38ab49b98ff933b935be699dd
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-11 15:51:07 -0500

    added className so that it can be extended by SC

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = (props) => {
-  return <StyledButton onClick={props.clicked}>{props.children}</StyledButton>;
+  return <StyledButton className={props.className} onClick={props.clicked}>{props.children}</StyledButton>;
 };
 
 export default Button;
