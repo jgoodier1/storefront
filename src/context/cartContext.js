b2eb50a6e21a3d268b8a39b3b753ef15ee546d94
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-12 15:51:30 -0500

    now works when isn't already a cart in storage

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const cartQuantity = () => {
   const cart = JSON.parse(sessionStorage.getItem('cart'));
+  if (cart === null) {
+    return 0;
+  }
   const quantity = cart.products.map(p => p.quantity);
   return quantity.reduce((a, b) => a + b);
 };
