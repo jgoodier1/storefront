4f9d79a88247422c3e0d6eed3e3e31234c66e5f1
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-13 16:07:59 -0500

    moved the product component to its own file

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,72 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useLocation, useHistory, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
 
+import Product from '../components/Product';
 import Spinner from '../components/Spinner';
-import Button from '../components/Button';
-import { addToCart } from '../utils/addToCart';
 import Modal from '../components/Modal';
-import CartContext from '../context/cartContext';
-
-const Product = props => {
-  const cartContext = useContext(CartContext);
-  const location = useLocation(); //might not be the best place for this
-  const history = useHistory();
-
-  // const clickDeleteHandler = () => {
-  //   console.log(props.id);
-  //   const deletedProduct = { id: 123456789022 /*props.id */ };
-  //   axios
-  //     .post('/delete-product/', deletedProduct, {
-  //       headers: { Authorization: 'bearer ' + localStorage.getItem('token') }
-  //     })
-  //     .then(res => {
-  //       console.log('Delete res.data', res.data);
-  //     })
-  //     .then(() => {
-  //       history.push('/products');
-  //     })
-  //     .catch(err => console.log(err.response.data));
-  // };
-
-  const addToCartHandler = () => {
-    addToCart(props.id, props.price, 1, cartContext.updateQuantity);
-    history.push('/cart');
-  };
-
-  let buttons = undefined;
-  if (location.pathname === '/products') {
-    buttons = <Button clicked={addToCartHandler}>ADD TO CART</Button>;
-  } else if (location.pathname === '/admin/products') {
-    buttons = (
-      <div>
-        <StyledLink to={{ pathname: '/admin/edit-product', search: '?id=' + props.id }}>
-          Edit
-        </StyledLink>
-        {/* <Button clicked={clickEditHandler}>Edit</Button> */}
-        <Button clicked={() => props.delete(props.id)}>Delete</Button>
-      </div>
-    );
-  }
-
-  let shortenedDesc;
-  if (props.description.length > 100) {
-    shortenedDesc = props.description.slice(0, 100).concat('...');
-  }
-
-  return (
-    <StyledProductDiv>
-      <StyledImg src={props.img} alt={props.title} />
-      <StyledAnchor href={'/products/' + props.id}>
-        {props.title.toUpperCase()}
-      </StyledAnchor>
-      <StyledDesc>{shortenedDesc}</StyledDesc>
-      <StyledPrice>$ {props.price}</StyledPrice>
-      {buttons}
-    </StyledProductDiv>
-  );
-};
 
 const Products = props => {
   const [products, setProducts] = useState([]);
@@ -154,24 +93,6 @@ const Products = props => {
 
 export default Products;
 
-const StyledLink = styled(Link)`
-  padding: 0.5rem;
-  font-weight: bold;
-  font-size: 1rem;
-  font-family: inherit;
-  background-color: white;
-  border: 2px solid #000;
-  color: #000;
-  text-decoration: none;
-  margin-right: 20px;
-
-  &:hover {
-    background-color: #000;
-    color: white;
-    cursor: pointer;
-  }
-`;
-
 const StyledProductsDiv = styled.div`
   ${'' /* margin: 56px; */}
   margin-left: 25px;
@@ -182,61 +103,6 @@ const StyledProductsDiv = styled.div`
   @media (max-width: 768px) {
     margin: 0;
   }
-`;
-
-const StyledProductDiv = styled.div`
-  ${'' /* background-color: #f4f4f4;
-  width: 250px;
-  padding: 15px;
-  border: 4px solid #bebebe; */}
-  padding: 2.5rem 0;
-  border-bottom: 1px solid #888383;
-  display: grid;
-  ${'' /* grid-template-columns: 20rem 1fr;
-  grid-template-rows: repeat(4, 1fr); */}
-  grid-column: 2/3;
-  grid-template-areas:
-    'img img title'
-    'img img desc'
-    'img img desc'
-    'img img price'
-    'img img bttn';
-`;
-
-const StyledAnchor = styled.a`
-  font-size: 1em;
-  margin: 0.5rem 0;
-  ${'' /* grid-column: 2/3; */}
-  grid-area: title;
-  text-decoration: none;
-  color: #000;
-  font-weight: bold;
-  font-size: 20px;
-`;
-
-const StyledImg = styled.img`
-  ${'' /* height: 200px; */}
-  width: 100%;
-  min-width: 10rem;
-  ${'' /* grid-column: 1/2; */}
-  ${'' /* grid-row: 1/5; */}
-  grid-area: img;
-  margin-right: 1rem;
-`;
-
-const StyledDesc = styled.p`
-  ${'' /* grid-column: 2/3; */}
-  grid-area: desc;
-  font-style: italic;
-  color: #4f4f4f;
-`;
-
-const StyledPrice = styled.p`
-  font-size: 1.5em;
-  font-weight: bold;
-  margin: 0.5rem;
-  ${'' /* grid-column: 2/3; */}
-  grid-area: price;
 `;
 
 const StyledButton = styled.button`
