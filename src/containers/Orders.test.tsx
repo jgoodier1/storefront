46f864add106e4a599c7dd56f090c2baec30a54c
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-30 11:42:11 -0500

    finished writing tests

--- src/containers/Orders.test.tsx
+++ src/containers/Orders.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { findByText, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Orders from './Orders';
 
@@ -12,7 +13,7 @@ const orders = [
     _id: '5fb439901315cb25852ca5ab',
     products: [
       {
-        _id: '5fb3fa46760c9c1135ca858d',
+        _id: '5fb3fa46760c9c1135ca858c',
         price: 12,
         prodId: {
           _id: '5fb3fa46760c9c1135ca858c',
@@ -26,7 +27,7 @@ const orders = [
         quantity: 1
       },
       {
-        _id: '5fb3fa46760c9c1135ca858c',
+        _id: '5fb3fa46760c9c1135ca858d',
         price: 9,
         prodId: {
           _id: '5fb3fa46760c9c1135ca858d',
@@ -54,53 +55,6 @@ const orders = [
       streetAddressTwo: ''
     },
     shippingSpeed: 'normal'
-  },
-  {
-    _id: '5fbd2169dc3af70843bfea46',
-    products: [
-      {
-        _id: '5fb3fa46760c9c1135ca858d',
-        price: 12,
-        prodId: {
-          _id: '5fb3fa46760c9c1135ca858c',
-          description:
-            'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
-          image: 'https://picsum.photos/400',
-          price: 280,
-          title: 'Fantastic Wooden Mouse',
-          userId: '5f57a5bfafa31a0d638157d9'
-        },
-        quantity: 1
-      },
-      {
-        _id: '5fb3fa46760c9c1135ca858c',
-        price: 9,
-        prodId: {
-          _id: '5fb3fa46760c9c1135ca858d',
-          description:
-            'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
-          image: 'https://picsum.photos/400',
-          price: 10,
-          title: 'Rustic Granite Chicken',
-          userId: '5f57a5bfafa31a0d638157d9'
-        },
-        quantity: 1
-      }
-    ],
-    totalPrice: 21,
-    createdAt: '2020-11-18T20:23:34.365+00:00',
-    contactInfo: {
-      city: 'Toronto',
-      country: 'Canada',
-      firstName: 'Bob',
-      lastName: 'Bobson',
-      phoneNumber: '4161234567',
-      postalCode: 'T0R0N0',
-      province: 'Ontario',
-      streetAddress: '123 Bob Street',
-      streetAddressTwo: ''
-    },
-    shippingSpeed: 'normal'
   }
 ];
 
@@ -114,7 +68,94 @@ describe('Orders', () => {
       </MemoryRouter>
     );
 
-    expect(await screen.findAllByText(/fantastic/i)).toHaveLength(2);
+    expect(await screen.findByText(/fantastic/i)).toBeInTheDocument;
+    expect(await screen.findByText(/rustic/i)).toBeInTheDocument;
+    expect(await screen.findByText(/9/i)).toBeInTheDocument;
+    expect(await screen.findByText(/12/i)).toBeInTheDocument;
+    expect(await screen.findAllByText(/Qty: 1/i)).toHaveLength(2);
+    expect(await screen.findByText(/bob/i)).toBeInTheDocument;
+    // screen.debug();
+  });
+
+  test('contact data appears when hovering "Ship To"', async () => {
+    axios.post.mockImplementationOnce(() => Promise.resolve({ data: orders }));
+
+    render(
+      <MemoryRouter>
+        <Orders showModal={() => {}} isLoggedIn={true} />
+      </MemoryRouter>
+    );
+
+    userEvent.hover(await screen.findByText(/ship/i));
+    expect(screen.getByText(/toronto/i)).toBeInTheDocument;
+    expect(screen.getByText(/ontario/i)).toBeInTheDocument;
+    expect(screen.getByText(/T0R0N0/)).toBeInTheDocument;
+    expect(screen.getByText(/Canada/i)).toBeInTheDocument;
+    userEvent.unhover(screen.getByText(/ship/i));
+    expect(screen.queryByText(/toronto/i)).not.toBeInTheDocument;
+    expect(screen.queryByText(/ontario/i)).not.toBeInTheDocument;
+    expect(screen.queryByText(/T0R0N0/)).not.toBeInTheDocument;
+    expect(screen.queryByText(/Canada/i)).not.toBeInTheDocument;
+    // screen.debug();
+  });
+
+  test('links go to product pages', async () => {
+    axios.post.mockImplementationOnce(() => Promise.resolve({ data: orders }));
+
+    render(
+      <MemoryRouter>
+        <Orders showModal={() => {}} isLoggedIn={true} />
+      </MemoryRouter>
+    );
+
+    await screen.findByText(/rustic/i);
+    expect(screen.getByText(/rustic/i).closest('a')).toHaveAttribute(
+      'href',
+      '/products/5fb3fa46760c9c1135ca858d'
+    );
+    expect(screen.getByText(/wooden/i).closest('a')).toHaveAttribute(
+      'href',
+      '/products/5fb3fa46760c9c1135ca858c'
+    );
+    // screen.debug();
+  });
+
+  test('renders error when fetch fails', async () => {
+    axios.post.mockImplementationOnce(() => Promise.reject(new Error('failed')));
+
+    render(
+      <MemoryRouter>
+        <Orders showModal={() => {}} isLoggedIn={true} />
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByText(/cannot load orders/i)).toBeInTheDocument;
+    // screen.debug();
+  });
+
+  test('closes modal when user click the "X" button', async () => {
+    axios.post.mockImplementationOnce(() => Promise.reject(new Error('failed')));
+
+    render(
+      <MemoryRouter>
+        <Orders showModal={() => {}} isLoggedIn={true} />
+      </MemoryRouter>
+    );
+
+    userEvent.click(await screen.findByText(/x/i));
+    expect(screen.queryByText(/cannot load orders/i)).not.toBeInTheDocument;
     screen.debug();
+  });
+
+  test('prompts user to log in when unauthenticated', () => {
+    render(
+      <MemoryRouter>
+        <Orders showModal={() => {}} isLoggedIn={false} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/please sign in/i)).toBeInTheDocument;
+    expect(screen.getByRole('button', { name: /sign in/i })).toBeInTheDocument;
+    // screen.debug();
   });
 });
