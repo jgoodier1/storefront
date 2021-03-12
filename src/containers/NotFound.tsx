0a424b5aedaa2f91fefd378da6857f7a6665e279
Jacob Goodier <jgoodier21@gmail.com>
2021-03-12 14:06:56 -0500

    Changed to a real emoji and centered text

--- src/containers/NotFound.tsx
+++ src/containers/NotFound.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const NotFound: React.FC = () => {
   return (
-    <main>
-      <h1>This Page Does Not Exist :O</h1>
-    </main>
+    <Container>
+      <h1>This Page Does Not Exist 😧</h1>
+    </Container>
   );
 };
 
 export default NotFound;
+
+const Container = styled.main`
+  display: grid;
+  justify-content: center;
+`;
