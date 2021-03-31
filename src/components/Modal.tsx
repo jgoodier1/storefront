560c53ead3de8d8b67b900013454230f3e5ac875
Jacob Goodier <jgoodier21@gmail.com>
2021-03-31 15:32:56 -0400

    moved modal style to sc and updated positioning

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Backdrop from './Backdrop';
-import classes from '../css/Modal.module.css';
 import { useHistory } from 'react-router-dom';
 
 interface ModalProps {
   show: boolean;
   children: React.ReactNode;
   modalClosed?: () => void;
+}
+
+interface ContainerProps {
+  show: boolean;
 }
 
 const Modal: React.FC<ModalProps> = props => {
@@ -24,18 +27,12 @@ const Modal: React.FC<ModalProps> = props => {
         show={props.show}
         clicked={props.modalClosed ? props.modalClosed : modalClosed}
       />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-150vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
+      <Container show={props.show}>
         <CloseButton onClick={props.modalClosed ? props.modalClosed : modalClosed}>
           X
         </CloseButton>
         {props.children}
-      </div>
+      </Container>
     </>
   );
 };
@@ -45,6 +42,27 @@ export default React.memo(
   (prevProps, nextProps) =>
     nextProps.show === prevProps.show && nextProps.children === prevProps.children
 );
+
+const Container = styled.div<ContainerProps>`
+  position: fixed;
+  z-index: 500;
+  background-color: white;
+  width: 70%;
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 1px black;
+  padding: 16px;
+  left: 0;
+  top: 80px;
+  right: 0;
+  margin: 0 auto;
+  transition: all 0.3s ease-out;
+  transform: ${props => (props.show ? 'translateY(0)' : 'translateY(-150vh)')};
+  opacity: ${props => (props.show ? '1' : '0')};
+
+  @media (min-width: 600px) {
+    width: 500px;
+  }
+`;
 
 const CloseButton = styled.button`
   position: fixed;
@@ -57,4 +75,5 @@ const CloseButton = styled.button`
   font-weight: bold;
   background-color: white;
   cursor: pointer;
+  width: fit-content;
 `;
